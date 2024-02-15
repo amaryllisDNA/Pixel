ed5f6f5db714d047cd22a3c50cd9b40e42024459
Anthéa von Borowski <82022008+amaryllisDNA@users.noreply.github.com>
2024-02-15 14:55:21 +0100

    update - coloring works

--- src/App.js
+++ src/App.js
@@ -5,43 +5,63 @@ import { Component } from 'react';
 class Pixel extends Component{
   constructor(props){
     super(props);
-    this.state = {
-      fill : "white", 
-      column : this.props.col,
-      line : this.props.lin,
+    this.state = { 
       border : "0.5px solid rgb(237, 231, 231)"
     }}
 
-    selectPixel = (col,lin) => {
-      console.log("you selected pixel"+ col +"," +lin);
-      this.setState({ border: "2px solid black" })   
+    selectPixel = () => {
+      console.log('you selected pixel ' + this.props.col + ', ' + this.props.lin, this.props.clr);
+      this.setState({ border: '2px solid black' });
+      this.props.onSelectPixel(this.props.col, this.props.lin);
+    };
+  
+
+    
+  render() {
+    return (
+      <div
+        className="pixel"
+        onClick={this.selectPixel}
+        style={{ backgroundColor: this.props.clr, border: this.state.border }}
+      ></div>
+    );
+  }
 }
 
-    render(){
-      return(
-        <div className="pixel" onClick={()=>this.selectPixel(this.props.col,this.props.lin)} style = {{backgroundColor:this.state.fill, border : this.state.border}}></div>
-      )
-    }
-    
-  
-}
 
 class Canva extends Component {
   constructor(props){
     super(props); 
     this.state = {
       table : Array.from({ length: 100  }, (_, rowIndex) => Array.from({ length: 100 }, (_, subArrayIndex) => [rowIndex, subArrayIndex])),
+      edited : this.props.edited
     }
   }
 
-  render(){
-    let table = this.state.table;
+  componentDidUpdate(prevProps) {
+    if (this.props.submitted !== prevProps.submitted) {
+      const table = [...this.state.table];
+      if (this.props.submitted) {
+        const [col, lin, color] = this.props.submitted;
+        table[col][lin] = [col, lin, color];
+      }
+      this.setState({ table });
+    }
+  }
 
+
+  render(){
+ 
+    const { onSelectPixel } = this.props;
+
+    
     return (
-      <div className = "canva"> {table.map((row, rowIndex) => (
+      <div className = "canva"> {this.state.table.map((row, rowIndex) => (
         <div key={rowIndex} className="row">
-          {row.map(([col, lin], subArrayIndex) => (
-            <Pixel key={`${rowIndex}-${subArrayIndex}`} col={col} lin={lin} />
+          {row.map(([col, lin, clr], subArrayIndex) => (
+            <Pixel key={`${rowIndex}-${subArrayIndex}`} 
+                  col={col} lin={lin} clr= {clr}
+                  onSelectPixel={onSelectPixel}  />
           ))}
 
 
@@ -58,29 +78,56 @@ class Gameboard extends Component {
     super(props); 
     this.state = {
       pixelchosen : undefined,
-      colorchosen : undefined
+      colorchosen : undefined, 
+      submitted : undefined,
+
     }
   }
+
+  handleSubmit = () => {
+    const { pixelchosen, colorchosen } = this.state;
+    if (pixelchosen && colorchosen) {
+      this.setState({ 
+        submitted: pixelchosen.concat(colorchosen),
+        pixelchosen: undefined,
+        colorchosen: undefined
+   }, () => {
+      console.log(this.state.submitted);
+        console.log("You changed the color of the pixel ", this.state.submitted[0], ",", this.state.submitted[1], "!");
+      });
+    }
+    else{
+      console.log("You need to select a color and a pixel ! ")
+    }
+  };
+
+  handleSelectPixel = (col, line) => {
+    this.setState({ pixelchosen: [col, line] });
+  };
+
+  handleSelectColor = (color) => {
+    this.setState({ colorchosen: color });
+  };
 
 
   render(){
     return(
       <div className='game'>
       
-        <Canva/>
+        <Canva onSelectPixel={this.handleSelectPixel} submitted = {this.state.submitted} />
         <div id="colorpalette">
-          <div className="coloring" col="#FFFFFF" style ={{backgroundColor:'#FFFFFF'}}></div>
-          <div className="coloring" col="#000000" style ={{backgroundColor:'#000000'}}></div>
-          <div className="coloring" col="#FF0000" style ={{backgroundColor:'#FF0000'}}></div>
-          <div className="coloring" col="#00FF00" style ={{backgroundColor:'#00FF00'}}></div>
-          <div className="coloring" col="#0000FF" style ={{backgroundColor:'#0000FF'}}></div>
-          <div className="coloring" col="#FFFF00" style ={{backgroundColor:'#FFFF00'}}></div>
-          <div className="coloring" col="#00FFFF" style ={{backgroundColor:'#00FFFF'}}></div>
-          <div className="coloring" col="#FF00FF" style ={{backgroundColor:'#FF00FF'}}></div>
-          <div className="coloring" col="#808080" style ={{backgroundColor:'#808080'}}></div>
-          <div className="coloring" col="#A52A2A" style ={{backgroundColor:'#A52A2A'}}></div>
+          <div className="coloring" onClick={() => this.handleSelectColor("#FFFFFF")} style={{ backgroundColor: '#FFFFFF' }}></div>
+          <div className="coloring" onClick={() => this.handleSelectColor("#000000")} style={{ backgroundColor: '#000000' }}></div>
+          <div className="coloring" onClick={() => this.handleSelectColor("#FF0000")} style={{ backgroundColor: '#FF0000' }}></div>
+          <div className="coloring" onClick={() => this.handleSelectColor("#00FF00")} style={{ backgroundColor: '#00FF00' }}></div>
+          <div className="coloring" onClick={() => this.handleSelectColor("#0000FF")} style={{ backgroundColor: '#0000FF' }}></div>
+          <div className="coloring" onClick={() => this.handleSelectColor("#FFFF00")} style={{ backgroundColor: '#FFFF00' }}></div>
+          <div className="coloring" onClick={() => this.handleSelectColor("#00FFFF")} style={{ backgroundColor: '#00FFFF' }}></div>
+          <div className="coloring" onClick={() => this.handleSelectColor("#FF00FF")} style={{ backgroundColor: '#FF00FF' }}></div>
+          <div className="coloring" onClick={() => this.handleSelectColor("#808080")} style={{ backgroundColor: '#808080' }}></div>
+          <div className="coloring" onClick={() => this.handleSelectColor("#A52A2A")} style={{ backgroundColor: '#A52A2A' }}></div>
         </div>
-        <button type='submit'>Submit</button>
+        <button type='submit' id="submit" onClick={this.handleSubmit}>Submit</button>
       </div>
     )
   }
